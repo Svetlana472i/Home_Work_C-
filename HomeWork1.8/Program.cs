a3d701b289e446bd32b13bfa884935e4214c070e
Svetlana472i <tito472i@mail.ru>
2022-09-02 23:43:37 +0400

    add even numbers

---
+++ HomeWork1.8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+while (even < number | even <= number)
+{
+    Console.Write(even);
+    Console.Write(", ");
+    even = even + 2;
+}
