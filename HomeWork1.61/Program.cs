e06035958ab9475739bb8683d06b973bddf2ff95
Svetlana <tito472i@mail.ru>
2022-09-04 13:03:52 +0300

    even number

---
+++ HomeWork1.61/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int rezult = number % 2;
+if (rezult==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
