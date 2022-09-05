990691f49f8655aeed4b519898e70ba572b6a265
Svetlana <tito472i@mail.ru>
2022-09-05 11:47:40 +0300

    sdd Example 13

---
+++ HomeWork2/13/Program.cs
@@ -0,0 +1,25 @@
+﻿int PullThird(int number)
+{
+    if (number / 100 < 1)
+    {
+        int result = -1;
+        return result;
+    }
+     else
+     {
+        int result = number % 10;
+        return result;
+     }
+ }
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ThirdNumber = PullThird(number);
+if (ThirdNumber == -1)
+{
+    Console.WriteLine("Третей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {ThirdNumber}");
+}
