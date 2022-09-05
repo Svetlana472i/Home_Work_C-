8850264616a4d188aaefff63b0ea9752d8320cd6
Svetlana <tito472i@mail.ru>
2022-09-05 12:08:14 +0300

    add Example 15

---
+++ HomeWork2/15/Program.cs
@@ -0,0 +1,23 @@
+﻿bool PullWeekEnd(int number)
+{
+    if (number == 6 | number == 7)
+    {
+        return true;
+    }
+     else
+     {
+        return false;
+     }
+ }
+
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+bool WeekEnd = PullWeekEnd(number);
+if (WeekEnd == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
