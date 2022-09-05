0047e2dbdf9bf3553be508fc3f1cda1da7ebb3d4
Svetlana <tito472i@mail.ru>
2022-09-05 10:56:44 +0300

    add example10

---
+++ HomeWork2/10/Program.cs
@@ -0,0 +1,10 @@
+﻿int PullSecond(int number)
+{
+     int result = (number / 10) % 10;
+     return result;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber = PullSecond(number);
+Console.WriteLine(secondNumber);
