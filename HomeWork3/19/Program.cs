18eec69faa87c3ab3cc969c41ed0cf813b3923b6
Svetlana <tito472i@mail.ru>
2022-09-07 15:01:23 +0300

    add test palindrom

---
+++ HomeWork3/19/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите пятизначное положительное число N");
+string N = Console.ReadLine();
+
+string ToPal(string n)
+{
+    string pal = string.Empty;
+    int length = n.Length;
+    int i = length;
+    for (i = length-1; i >= 0; i--)
+    {
+       pal = pal + $"{n[i]}";
+    }
+    return pal;
+}
+
+string result = ToPal(N);
+
+if (N == result)
+    {
+       Console.WriteLine("Да");
+    }
+else
+    {
+       Console.WriteLine("Нет");
+    }
