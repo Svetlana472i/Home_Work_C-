7c0ab672280ab5d27f958f62616e2cb92f1069d7
Svetlana <tito472i@mail.ru>
2022-09-07 21:18:24 +0300

    add sum number

---
+++ HomeWork4/27/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите Число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+if (!isNumber || number < 0)
+{
+    Console.WriteLine("Символы, введенны неверно");
+    return;
+}
+int GetSumNumber(int number)
+{
+    int i = 0;
+    int result = 0;
+    if (number == 0)
+    {
+        return 0;
+    }
+    else 
+    while (number != 0)
+    {
+        i++;
+        result = result + number % 10;
+        number = number / 10;
+    }
+return result;
+}
+
+int sum = GetSumNumber(number);
+Console.WriteLine(sum);
