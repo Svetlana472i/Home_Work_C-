681c16f9a51e880b2d298df3f8dc8adf8e44eca6
Svetlana <tito472i@mail.ru>
2022-09-07 20:52:46 +0300

    add degree number

---
+++ HomeWork4/25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите Число A");
+bool isNumberA = double.TryParse(Console.ReadLine(), out double a);
+Console.WriteLine("Введите Число B");
+bool isNumberB = int.TryParse(Console.ReadLine(), out int b);
+if (!isNumberA || !isNumberB || b < 0)
+{
+    Console.WriteLine("Символы, введенны неверно");
+    return;
+}
+double Degree(double a, int b)
+{
+    double result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+Console.WriteLine(Degree(a, b));
